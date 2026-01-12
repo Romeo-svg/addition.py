2d2372d8e2ec2cf67af4575128766b4aad43d13b
Roney Mwangi <roneymwangi24@gmail.com>
2026-01-12 08:58:03 +0300

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,7 @@
+#addition usind def functions
+def add(a,b):
+    return a+b
+num1=float(input("Enter Fisrt Number: "))
+num2=float(input("Enter Second Number: "))
+result=add(num1,num2)
+print(result)
